b592ab52f30cd4c7d4060139284121a705f02638
meamuri <meammf@yandex.ru>
2017-07-22 14:40:01 +0300

    spark streaming context was configured

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 // библиотеки Spark + MongoDB
-libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0" % "provided"
-libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.1"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0" // % "provided"
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.2.0"
 libraryDependencies += "org.mongodb" %% "casbah" % "3.1.1"
 
 // библиотеки тестирования
--- src/main/scala/factories/StreamingFactory.scala
+++ src/main/scala/factories/StreamingFactory.scala
@@ -5,10 +5,9 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object StreamingFactory {
   private val conf = new SparkConf()
-    .setMaster("local")
+    .setMaster(Resources.getSparkLocalConfig)
     .setAppName("streaming-population-data")
     .set("spark.cores.max", Resources.getSparkCoreCountConfig)
-    .set("spark.master", Resources.getSparkLocalConfig)
     .set("spark.executor.memory", Resources.getSparkMemoryConfig)
 
   private val ssc = new StreamingContext(conf, Seconds(Resources.getStreamPeriod))
