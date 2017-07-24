464afbafab1122c929c9ec2ce84bcd4630e9e9fe
meamuri <meammf@yandex.ru>
2017-07-24 21:52:20 +0300

    JobRatio placeholder was added

---
+++ src/main/scala/application/JobRatio.scala
@@ -0,0 +1,28 @@
+package application
+
+import factories.{Resources, StreamingFactory}
+import services.Miner
+import utils.{Converter, SparkUtils}
+
+object JobRatio {
+  def main(args: Array[String]): Unit = {
+    val ssc = StreamingFactory.getStreamingContext
+    ssc.checkpoint(Resources.getStreamCheckpointPath)
+
+    val lines = ssc.socketTextStream(Resources.getHost, Resources.getStreamPort)
+    val ds = Converter.linesToCities(lines)
+    val cities = ds
+      .map(city => (city.name, city))
+
+    val selected_rows = cities.updateStateByKey(SparkUtils.recentlyCities)
+
+    val countries = selected_rows
+      .map(pair => (pair._2.country, pair._2))
+
+   countries.print()
+
+    ssc.start()
+    ssc.awaitTerminationOrTimeout(5*1000) // 5 sec
+
+  }
+}
